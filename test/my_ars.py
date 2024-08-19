12c474dbe9e7bbe5d563f788c289c04a1e405748
Andrew Brady <handy@hackerhomestead.com>
2024-08-19 11:22:18 -0500

    new tests with ack ars

--- test/my_ars.py
+++ test/my_ars.py
@@ -1,4 +1,9 @@
 import socketserver
+import datetime
+import binascii
+
+OUTBOUND_INTERFACE = b'enx0a003e6acab4'
+
 
 class MyUDPHandler(socketserver.BaseRequestHandler):
     """
@@ -11,11 +16,22 @@ class MyUDPHandler(socketserver.BaseRequestHandler):
     def handle(self):
         data = self.request[0].strip()
         socket = self.request[1]
-        print("{} wrote:".format(self.client_address[0]))
-        print(data)
+
+        ct = datetime.datetime.now()
+
+        print("{}\t{}\twrote: {}\t({})".format( ct, self.client_address[0], data,binascii.hexlify(data)))
+
+        ## Not sure why these are here but I suspect that they are part of an ack
         #.decode(encoding='UTF-8')
         #socket.sendto(data.upper(), self.client_address)
-        socket.sendto(data, self.client_address)
+
+        # Let's give it a go
+        # Except arent we just echoing back? 
+
+        # Disabled to see what happens
+        ACK = b'\x00\x01\x3f\x00\x00'
+        socket.sendto(ACK, self.client_address)
+        print("\tSent ACK {} to {}".format(binascii.hexlify(ACK), self.client_address))
 
 if __name__ == "__main__":
     HOST, PORT = "192.168.10.2", 4005
