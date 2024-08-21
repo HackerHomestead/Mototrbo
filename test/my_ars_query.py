05f4afe00cbb56378800c4bbec3c26d3e454dbfb
Andrew Brady <handy@hackerhomestead.com>
2024-08-21 13:44:39 -0500

    Sloppy argsv for sending ars query

--- test/my_ars_query.py
+++ test/my_ars_query.py
@@ -8,19 +8,16 @@ import sys
 from Protocol import *
 
 protocol = Protocol()
-OUTBOUND_INTERFACE = b'enx0a003ec8e4e3' 
 OUTBOUND_INTERFACE = b'enx0a003e2044dd'
-TARGET_RADIO_ID = 5050
+TARGET_RADIO_ID = int(sys.argv[1])
 CAI = 12
 
-UDP_IP = "12.0.35.130" # Radio ID 9090
-UDP_IP = "12.0.19.186" # Radio ID 5050
+UDP_IP = protocol.id2ip(CAI, TARGET_RADIO_ID)
 UDP_PORT = 4005
 
 # This is a example message MESSAGE = b'\x00\x12\xe0\x00\x88\x04\r\x00\n\x00h\x00e\x00l\x00l\x00o\x00'
 MESSAGE = b'\x00\x01\x74' # ARS Query
 
-print ("UDP target IP:", UDP_IP)
 print ("Target Radio ID", protocol.ip2id(UDP_IP), " -> ", protocol.id2ip(CAI, TARGET_RADIO_ID))
 print ("UDP target port:", UDP_PORT)
 print ("message:", binascii.hexlify(MESSAGE))
